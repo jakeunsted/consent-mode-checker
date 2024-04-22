f98b6d3e5271824863f3eb92437c6facf304396a
Jake Unsted <jake.unsted@gmail.com>
2024-04-22 17:41:44 +0100

    change analytics url

--- src/methods/scalper.js
+++ src/methods/scalper.js
@@ -20,7 +20,7 @@ const scalper = async (url) => {
     console.log('Request URL:', request.url());
     if (
       request.url().startsWith('https://region1.google-analytics.com/g/collect') ||
-      request.url().startsWith('https://google-analytics.com/g/collect'))
+      request.url().startsWith('https://www.google-analytics.com/g/collect'))
     {
       const postData = await request.url().split('?')[1];
       payloads.push(postData);
