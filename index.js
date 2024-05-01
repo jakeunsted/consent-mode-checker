206c019a248a66a9108460d7704a3bc9946e4b96
Jake Unsted <jake.unsted@gmail.com>
2024-05-01 16:04:14 +0100

    cors trial error

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ if (process.env.RUN_IN_DOCKER === 'false') {
       return {
         statusCode: 200,
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
           "Access-Control-Allow-Methods": "POST, OPTIONS",
           "Access-Control-Allow-Headers": "Content-Type",
         },
